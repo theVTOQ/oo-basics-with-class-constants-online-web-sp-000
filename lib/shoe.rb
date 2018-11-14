95b39d6a72bc8118852bbaf6294ece547fe8c0e8
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-14 21:02:49 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    BRANDS << brand if !BRANDS.include?(brand)
   end
 
   def cobble
@@ -16,6 +16,6 @@ class Shoe
 
   def brand=(brand)
     @brand = brand
-    BRANDS << brand
+    BRANDS << brand !BRANDS.include?(brand)
   end
 end
